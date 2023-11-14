99e47b7e4be20076a4a13b95276eb7babe70ed24
João Paulo Valadares <97069276+jotapvaladaresdev@users.noreply.github.com>
2023-11-14 17:29:52 -0300

    Delete script.js
    adsadssd

--- script.js
+++
@@ -1,8 +0,0 @@
-// Adicione o código JavaScript aqui se necessário
-// Por exemplo, você pode adicionar um evento de clique ao botão de download
-// para iniciar o download da planilha.
-document.getElementById('downloadButton').addEventListener('click', function() {
-    // Lógica para iniciar o download da planilha
-    alert('Iniciar download da planilha');
-  });
-  

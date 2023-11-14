1902a266482fbe859986db3e0c8402e546507cd2
João Paulo Valadares <97069276+jotapvaladaresdev@users.noreply.github.com>
2023-11-14 17:30:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Adicione o código JavaScript aqui se necessário
+// Por exemplo, você pode adicionar um evento de clique ao botão de download
+// para iniciar o download da planilha.
+document.getElementById('downloadButton').addEventListener('click', function() {
+    // Lógica para iniciar o download da planilha
+    alert('Iniciar download da planilha');
+  });
+  
